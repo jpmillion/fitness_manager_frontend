007be727e389a8b2997d4a1064740b0cbd5702eb
john million <jpmillion@comcast.net>
2021-03-08 15:50:39 -0500

    define function get workouts

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('in index.js');
+const workoutEndPoint = "http://localhost:3000/api/v1/workouts";
+
+getWorkouts();
+
+function getWorkouts() {
+    fetch(workoutEndPoint)
+    .then(resp => resp.json())
+    .then(workouts => {
+        const body = document.querySelector('body');
+        workouts.data.forEach(workout => {
+            const div = document.createElement('div');
+            div.innerText = workout.attributes.name;
+            body.appendChild(div);
+        });
+    });
+}
