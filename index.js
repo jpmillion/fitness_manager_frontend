8ebe8d8e3344df5b05b1fe7c2cad80bc8df91de9
john million <jpmillion@comcast.net>
2021-03-08 19:44:50 -0500

    event listner for videos on click

--- index.js
+++ index.js
@@ -8,16 +8,29 @@ function getWorkouts() {
     .then(workouts => {
         const body = document.querySelector('body');
         const div = document.createElement('div');
-        const ol = document.createElement('ol');
+
         workouts.data.forEach(workout => {
+            const ol = document.createElement('ol');
+            ol.id = workout.id;
             div.innerText = workout.attributes.name;
+            div.appendChild(ol);
             body.appendChild(div);
         });
+
         workouts.included.forEach(exercise => {
+            const {name, video_url} = exercise.attributes;
             const li = document.createElement('li');
-            li.innerText = exercise.attributes.name;
-            ol.appendChild(li);
-            div.appendChild(ol);
+            li.id = name
+            li.innerText = name;
+            li.addEventListener('click', () => {
+                const frame = document.createElement('iframe');
+                frame.src = video_url;
+                frame.width = '460';
+                frame.height = '315';
+                frame.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+                div.appendChild(frame);
+            })
+            document.getElementById(exercise.relationships.workout.data.id).appendChild(li);
         });
     });
 }
