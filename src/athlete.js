74e1851cf7052c135bbcf6def19d8800d37c42dd
john million <jpmillion@comcast.net>
2021-03-19 19:22:17 -0400

    fetch to create athlete

--- src/athlete.js
+++ src/athlete.js
@@ -12,3 +12,25 @@ class Athlete {
 }
 
 Athlete.all = [];
+
+async function registerAthlete() {
+    displayAthleteDivs();
+    const name = document.getElementById('register').value;
+    const resp = await fetch(athleteEndPoint, {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name
+        })
+    });
+    const json = await resp.json();
+    new Athlete(json.data).renderAthlete();
+}
+
+function displayAthleteDivs() {
+    document.getElementById('athleteLogin').classList.add('d-none');
+    document.getElementById('athleteName').classList.remove('d-none');
+    document.getElementById('athleteWorkout').classList.remove('d-none');
+}
